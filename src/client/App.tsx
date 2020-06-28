886feee4e482022835e2eba733e92b658c7b4602
marezgui <m.rezgui@outlook.fr>
2020-06-28 19:46:05 +0200

    routes folder renamed to pages

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import Home from './routes/Home/Home';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
